a059544270cf70582b42d8095eb411618e2372ee
Roberto Cestari <robertotcestari@gmail.com>
2024-07-26 09:53:57 -0300

    force migration

--- deploy.php
+++ deploy.php
@@ -18,7 +18,7 @@ set('keep_releases', 1);
 task('api-commands', function () {
     run('{{bin/php}} {{release_path}}/artisan api:orders-api:reset');
     run('{{bin/php}} {{release_path}}/artisan api:frases-motivacionais:reset');
-    run('{{bin/php}} {{release_path}}/artisan migrate --database="olympic_games" --path="database/migrations/olympic_games"');
+    run('{{bin/php}} {{release_path}}/artisan migrate --database="olympic_games" --path="database/migrations/olympic_games" --force');
 })->desc('Reset orders');
 
 // Hosts
