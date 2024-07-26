93f29af64410e03e6ba61869a8622a090bf7963e
Roberto Cestari <robertotcestari@gmail.com>
2024-07-26 11:25:13 -0300

    redirect to docs

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Route;
 
 // create a home
 Route::get('/', function () {
-    return response()->json(['message' => 'Codante.io APIs Service. For docs, visit https://apis-docs.codante.io/']);
+    return redirect('https://docs.apis.codante.io');
 });
