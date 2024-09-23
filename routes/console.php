47a5062b12622131e61e169b2484220d6f93ffe3
Ícaro Harry <me@icaro.io>
2024-09-23 16:40:36 -0300

    Fix reviews-api scheduler

--- routes/console.php
+++ routes/console.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Schedule;
 Schedule::command('api:orders-api:reset')->hourly();
 Schedule::command('api:frases-motivacionais:reset')->hourly();
 Schedule::command('api:job-board:reset')->hourly();
-Schedule::command('api:reviews-api:reset')->daily();
+Schedule::command('api:reviews:reset')->everyTwoMinutes();
 
 Schedule::call(function () {
     $eventFetcher = new EventFetcher();
